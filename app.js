85dc2982755d2ea55d31f3b895aa7f840a8fbcc4
Adil Khosa <muhammadadilmemon79@gmail.com>
2024-09-26 15:24:54 +0500

    added files

---
+++ app.js
@@ -0,0 +1,153 @@
+// CLASS ASSIGNMENT:
+
+
+// ARRAYS
+
+// Q1:
+// var StudNameArr = [];
+
+// Q2:
+// var StudNameArr = [];
+
+// Q3:
+// var arr = ["apple","banana","mango"];
+
+// Q4:
+// var arr = [100,200,300];
+
+// Q5:
+// var arr = [false,true];
+
+// Q6:
+// var arr = ["adil",1200,true,undefined];
+
+// Q7:
+// var arr = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+// document.write(`<h1>Qualifications</h1> <br> ${arr[0]} <br> ${arr[1]} <br> ${arr[2]} <br> ${arr[3]} <br> ${arr[4]} <br> ${arr[5]} <br> ${arr[6]} <br> ${arr[7]}`);
+
+// Q8:
+// var studNames = ["ahmed","ali","farooq"];
+// var studMarks = [350,400,250];
+// var percentage = [350*100/500,400*100/500,250*100/500];
+// document.write(`Scores of ${studNames[0]} is ${studMarks[0]} Percentage: ${percentage[0]}% 
+//     <br> Scores of ${studNames[1]} is ${studMarks[1]} Percentage: ${percentage[1]}%
+//     <br> Scores of ${studNames[2]} is ${studMarks[2]} Percentage: ${percentage[2]}%
+//     `)
+
+// Q9:
+// var colorArr = ["red","yellow","blue","orange","green"];
+// colorArr.unshift(prompt("Enter a color to add in the beginning:"));
+// colorArr.push(prompt("Enter a color to add in the end:"));
+// colorArr.unshift("black","white");
+// colorArr.shift();
+// colorArr.pop();
+// colorArr.splice(+prompt("At which index:"),0,prompt("Enter color name:"));
+// colorArr.splice(+prompt("At which index:"),+prompt("And how many:"));
+// document.write(colorArr);
+
+// Q10:
+
+// Q11:
+// var citiesArr = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// var selCities = citiesArr.slice(2,4);
+// document.write(`Cities List: <br> ${citiesArr} <br> <br> Selected Cities List: <br> ${selCities}`);
+
+// Q12:
+
+// Q13:
+
+// Q14:
+
+// Q15:
+
+
+
+
+
+// ------------------------------------------------------------------------------------
+
+
+
+
+// HOME ASSIGNMENT:
+
+
+// Chapter 15 (Array I)
+
+// Q1:
+// var arr = [];
+
+// Q2:
+// var arr = ["adil"];
+
+// Q3:
+// var alphabet = ["h","i","j","k"];
+// alert(alphabet[2]);
+
+// Q4:
+// var alphabet = ["h","i","j","k","l","m","n","o"];
+// console.log(alphabet.length);
+
+// Q5:
+// var arr = ["apple"];
+// arr.push("mango");
+// alert(arr[1]);
+
+
+// Chapter 16 (Array II)
+
+// Q1:
+// var arr = ["lamborghini"];
+// arr.push("ferrari");
+// alert(arr[1]);
+
+// Q2:
+// var alphabet=["h","i","j","k"];
+// alphabet.pop();
+// document.write(alphabet);
+
+// Q3:
+// var alphabet=["h","i","j","k"];
+// alphabet.push(100);
+// document.write(alphabet);
+
+
+// Chapter 16 (Array III)
+
+// Q1:
+// var sizes = ["S","M","XL","XXL","XXXL"];
+// sizes.shift();
+// document.write(sizes);
+
+// Q2:
+// var sizes = ["S","M","XL","XXL","XXXL"];
+// sizes.unshift(100,200,300);
+// document.write(sizes);
+
+// Q3:
+// var arr = ["ali"];
+// arr.unshift("ahmed");
+// alert(arr[0]);
+
+// Q4:
+// var sizes = ["S","M","XL","XXL","XXXL"];
+// sizes.splice(2,0,"L");
+// document.write(sizes);
+
+// Q5:
+// var sizes = ["S","M","XL","XXL","XXXL"];
+// var regSizes = sizes.slice(0,3);
+// document.write(regSizes);
+
+// Q6:
+// var pets = ["dog","cat","ox","duck","frog"];
+// pets.splice(1,3,"fish","hen");
+// document.write(pets);
+
+// Q7:
+// var pets = ["dog","cat","ox","duck","frog"];
+// pets.splice(1,2);
+// document.write(pets);
+
+// Q8:
+// var pets = ["dog","cat","ox","duck","frog","flea"];
